71c2ba7434737b603cc800b917ac03369eb21c8c
Dimitri <dimitri@net-c.fr>
2016-09-19 14:06:32 +0200

    Update sudoku_main.c

--- sudoku_main.c
+++ sudoku_main.c
@@ -9,7 +9,7 @@ int 	error(void)
 
 int	main(int ac, char ** av)
 {
-	if (ac != 10 || valid_size(tab) || valid_line(av, 1) || valid_column(av, 0)
+	if (ac != 10 || valid_line(av, 1) || valid_column(av, 0)
 		|| valid_block(av, 0))
 		return (error());
 	resolver(av);
